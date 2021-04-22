7e3bcdbd1f68f7828e42de88260f82b01d6804b4
MaximilianGrieser <61865913+MaximilianGrieser@users.noreply.github.com>
2021-04-22 15:17:56 +0200

    Read Produkte from JSON File

--- public/src/app/produkte/produkte.component.ts
+++ public/src/app/produkte/produkte.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import  *  as  datafile  from  './produkte.json';
 
-export interface PeriodicElement {
+export interface Produkt {
   Bezeichnung: string,
   Herkunft: string,
   Kategorie: string,
@@ -18,16 +19,51 @@ export interface PeriodicElement {
 })
 
 export class ProdukteComponent implements OnInit {
+  iBezeichnung: string;
+  iHerkunft: string;
+  iKategorie: string;
+  iVerkaufspreis: number;
+  iLieferant: string;
+  iProduzent: string;
+  iAnzahl: number;
+
+  ELEMENT_DATA: Produkt[] = (datafile as any).default;
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log (this.ELEMENT_DATA);
+    this.dataSource = new MatTableDataSource(this.ELEMENT_DATA);
   }
 
-  ELEMENT_DATA: PeriodicElement[] = [
-    {Bezeichnung: "Gurke", Herkunft: "Deutschland", Kategorie: "Gemuese", Verkaufspreis: 5, Lieferant: "Thomas Müller", Produzent: "Theos Erdhof", Anzahl: 2}
-  ];
+  changeBezeichnung(event: Event) {
+    this.iBezeichnung = (event.target as HTMLInputElement).value;
+  }
+  changeHerkunft(event: Event) {
+    this.iHerkunft = (event.target as HTMLInputElement).value;
+  }
+  changeKategorie(event: Event) {
+    this.iKategorie = (event.target as HTMLInputElement).value;
+  }
+  changeVerkaufspreis(event: Event) {
+    this.iVerkaufspreis = parseInt((event.target as HTMLInputElement).value);
+  }
+  changeLieferant(event: Event) {
+    this.iLieferant = (event.target as HTMLInputElement).value;
+  }
+  changeProduzent(event: Event) {
+    this.iProduzent = (event.target as HTMLInputElement).value;
+  }
+  changeAnzahl(event: Event) {
+    this.iAnzahl = parseInt((event.target as HTMLInputElement).value);
+  }
   
+  add() {
+    this.ELEMENT_DATA.push(
+    {Bezeichnung: this.iBezeichnung, Herkunft: this.iHerkunft, Kategorie: this.iKategorie, Verkaufspreis: this.iVerkaufspreis, Lieferant: this.iLieferant, Produzent: this.iProduzent, Anzahl: this.iAnzahl})
+    console.log(this.ELEMENT_DATA)
+    this.dataSource = new MatTableDataSource(this.ELEMENT_DATA);
+  }  
 
   displayedColumns: string[] = ['Bezeichnung', 'Herkunft', 'Kategorie', 'Verkaufspreis', 'Lieferant', 'Produzent', 'Anzahl'];
   dataSource = new MatTableDataSource(this.ELEMENT_DATA);
